0849e1ebd2f81e475ae670345a15c34deef200fd
Peter Kravets <petr.kravetss@gmail.com>
2017-05-13 16:30:12 +0300

    Инициализация моделей и миграций

--- database/migrations/2017_05_13_130345_create_donations_table.php
+++ database/migrations/2017_05_13_130345_create_donations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateDonationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('donations', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('project_id');
+            $table->integer('user_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->integer('donate');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('donations');
     }
 }
